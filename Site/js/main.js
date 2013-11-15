43248b1c461c3086b999afa4fd1229d7e1cd3097
Sun Sophie <sunsophie@Suns-MacBook-Pro.local>
2013-11-14 19:24:56 -0500

    volume seekbar

--- Site/js/main.js
+++ Site/js/main.js
@@ -1,8 +1,12 @@
 var flashReady = function () {
-	// body...
+	flash.connect('rtmp://localhost/SMSServer');
+}
+
+var videoDuration;// body...
+	var totalDruation;
 
 	console.log('hello');
-	played();
+
 	$('.camera').click(function(){
 		var cameras = flash.getCameras();
 		for(var i=0; cameras.length >i; i++){
@@ -17,24 +21,39 @@ var flashReady = function () {
 		}
 		$(this).unbind();
 	})
-	function played(){
-		$('.play').click(function(){
-			$(this).unbind();
-			flash.connect('rtmp://localhost/SMSServer');
-			$(this).remove();
-			$('.volume').after('<div class="pause"></div>');
-			paused();
-		});
+
+	var playing = true;
+
+$('.play').click(function(){
+	// $(this).unbind();
+	// $(this).remove();
+	// $('.volume').after('<div class="pause"></div>');
+	// paused();
+	if(!playing){
+		flash.playPause();
+		$('.play').css('background-image','url("http://localhost:8888/Site/image/pauseBtn.png")');
+		console.log('playing')
+		playing= true;
+	}else{
+		flash.playPause();
+		$('.play').css('background-image', 'url("http://localhost:8888/Site/image/playBtn.png")');
+		playing = false;
 	}
-	function paused(){
-		$('.pause').click(function(){
-			// $(this).unbind();
-			flash.playPause();
-			// $(this).remove();
-			// $('.volume').after('<div class="play"></div>');
-			// played();
-		});
-	}
+});
+
+	// function played(){
+		
+
+	// }
+	// function paused(){
+	// 	$('.pause').click(function(){
+	// 		// $(this).unbind();
+	// 		flash.playPause();
+	// 		// $(this).remove();
+	// 		// $('.volume').after('<div class="play"></div>');
+	// 		// played();
+	// 	});
+	// }
 
 	var record=true;
 	$('.record').click(function(e){
@@ -48,24 +67,88 @@ var flashReady = function () {
 			record=true;
 			playRec='';
 		}
-	})
-}
+	});
 
-var playRec = '';
+	var getDuration = function(time){
+		videoDuration = time;
+		var videoDurationMin = Math.floor(time/60);
+		var videoDurationSec = Math.floor(time%60);
+		$('.duration p').text(totalDuration = videoDurationMin + ":" + videoDurationSec);
+		console.log('getDuration');
+	}
 
-var connected = function(success,error){
+	var seekTime = function(time){
+		var currentMin = Math.floor(time/60);
+		var currentSec = Math.floor(time%60);
 
-	console.log('success:',success);
-		console.log('error:',error);
-
-	if(success){
-		if(playRec==="rec"){
-			flash.startRecording('test',0,0);
+		if(currentSec<10){
+			$('.duration p').text(currentMin+ ':0' +currentSec + '/' + totalDuration);
 		}else{
-		flash.startPlaying('hobbit_vp6.flv');
-		};
-		
-	};
-		
-}
+			$('.duration p').text(currentMin + ':' + currentSec + '/' + totalDuration);
+		}
+	}
+
+	$('.timelineSeekbar').on('click',function(e){
+		var left = e.pageX-$(this).offset().left;
+		var perc = left/$('.timelineSeekbar').width();
+		var time = perc*videoDuration;
+		$('.timelineSeekbar img').css('margin-left', left+'px')
+
+		flash.setTime(time);
+		e.preventDefault();
+		console.log(perc);
+		return false;
+
+	})
+
+	$('.volSlider').on('click',function(e){
+		var left = e.pageX-$(this).offset().left;
+		var perc = left/$('.volSlider img').width();
+		$('.volSlider img').css('margin-left', left+'px')
+
+		flash.setVolume(perc);
+		e.preventDefault();
+		console.log(perc);
+		return false;
+
+	})
+
+	var playRec = '';
+	// var myDataRef = new Firebase('https://qccqxxmm8og.firebaseio-demo.com/');
+	// $('#messageInput').keypress(function (e) {
+	//         if (e.keyCode == 13) {
+	//           var name = $('#nameInput').val();
+	//           var text = $('#messageInput').val();
+	//           myDataRef.push('User ' + name + ' says ' + text);
+	//           $('#messageInput').val('');
+	//         }
+	//       });
+	// 	myDataRef.on('child_added', function(snapshot) {
+	//   //We'll fill this in later.
+	// 	});
+	// 	myDataRef.on('child_added', function(snapshot) {
+	//         var message = snapshot.val();
+	// displayChatMessage(message.name, message.text);
+	//       });
+	//       function displayChatMessage(name, text) {
+	//         $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+	//         $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+	//       };
+
+	var connected = function(success,error){
+
+		console.log('success:',success);
+			console.log('error:',error);
+
+		if(success){
+			if(playRec==="rec"){
+				flash.startRecording('test',0,0);
+			}else{
+			flash.startPlaying('hobbit_vp6.flv');
+			flash.playPause();
+			};
+			
+		};	
+			
+	}
 
